6dacbe294289a7a1382c9d411d8d3faecb16e7d7
pseudotensor <pseudotensor@gmail.com>
2017-03-26 14:49:28 -0400

    attempting to compile, but not sure what wrong after some fixes.

--- src/python/make.sh
+++ src/python/make.sh
@@ -8,29 +8,30 @@ cd src/python
 swig -python -c++ -I../ -outcurrentdir -o FAO_DAG_wrap.cu FAO_DAG.i
 
 nvcc -Xcompiler -fno-strict-aliasing,-fno-common,-dynamic,-fwrapv,-Wall,-Wstrict-prototypes \
--I/usr/local/include -I/usr/local/opt/sqlite/include \
--DNDEBUG -g -O3  -DLAPACK_LIB_FOUND \
--I/Users/stevend2/anaconda/envs/abs_ops/lib/python2.7/site-packages/numpy/core/include \
--I../ -I../../include/ -I../../include/scs/include/ \
--I../../include/pogs_fork/src/include/ \
--I/Users/stevend2/anaconda/envs/abs_ops/include/python2.7 \
--c FAO_DAG_wrap.cu \
--o FAO_DAG_wrap.o \
--std=c++11 -DFAO_GPU
+     -I/usr/local/include \
+     -DNDEBUG -g -O3  -DLAPACK_LIB_FOUND \
+     -I/usr/local/lib/python2.7/dist-packages/numpy/core/include \
+     -I/usr/include/python2.7 \
+     -I../ -I../../include/ -I../../include/scs/include/ \
+     -I../../include/pogs_fork/src/include/ \
+     -c FAO_DAG_wrap.cu \
+     -o FAO_DAG_wrap.o \
+     -std=c++11 -DFAO_GPU
 
-g++ -bundle -undefined dynamic_lookup \
--L/Users/stevend2/anaconda/envs/abs_ops/lib \
--arch x86_64 -L/usr/local/include \
--I/usr/local/include \
-FAO_DAG_wrap.o ../../include/pogs_fork/src/build/pogs.a \
--L ../../include/scs/out -lscsindir \
--L/usr/local/cuda/lib -L/usr/local/lib \
--lcudart -lcublas -lcusparse -lcufftw \
--L/usr/lib -L/usr/lib \
--L/Users/stevend2/anaconda/envs/abs_ops/lib \
--lfftw3 -lfftw3f -lfftw3l -lfftw3_threads \
--lfftw3f_threads -lfftw3l_threads \
--o _FAO_DAG.so
+#g++ -bundle -undefined dynamic_lookup \
+#-arch x86_64 -L/usr/local/include \
+g++  -I/usr/local/include \
+     $(python-config --cflags) \
+     $(python-config --ldflags) \
+     FAO_DAG_wrap.o ../../include/pogs_fork/src/build/pogs.a \
+     -L ../../include/scs/out -lscsindir \
+     -L/usr/local/cuda/lib64 -L/usr/local/lib \
+     -lcudart -lcublas -lcusparse -lcufftw \
+     -L/usr/lib -L/usr/lib \
+     -lfftw3 -lfftw3f -lfftw3l -lfftw3_threads \
+     -lfftw3f_threads -lfftw3l_threads \
+     -o _FAO_DAG.so
+
 # echo ' ' >> FAO_DAG.i
 # python setup.py install
 # python ../../test/test_pogs_mat_free.py
